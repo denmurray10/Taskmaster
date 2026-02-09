9366843bf0f23c42eeee28c329d2f735543250fb
phoenixfiretv <dennis.murray10@gmail.com>
2026-02-09 14:06:07 +0000

    Add database configuration and dependencies for PostgreSQL

--- Taskmaster/env.py
+++ Taskmaster/env.py
@@ -1,1 +1,2 @@
 SECRET_KEY = 'django-insecure-^16o%03r=@aktsf@$vvbccr-)dxaa$m_86v67%4&79uv(c-&3z'
+DATABASE_URL = 'postgresql://neondb_owner:npg_7BEvOGSUZY8R@ep-little-credit-aggzf1v2.c-2.eu-central-1.aws.neon.tech/polo_crown_frail_247096'
--- Taskmaster/settings.py
+++ Taskmaster/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from .env import SECRET_KEY
+from .env import SECRET_KEY, DATABASE_URL
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,10 +77,7 @@ WSGI_APPLICATION = 'Taskmaster.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(DATABASE_URL)
 }
 
 
